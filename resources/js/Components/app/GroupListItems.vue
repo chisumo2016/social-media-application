7d6fdf5aa5557551cf97e0ca32d429c5ad7dcf41
bernard chisumo <bchisumo74@gmail.com>
2024-02-25 18:35:45 +0000

    Fix the bugs on GroupListItems.vue file

--- resources/js/Components/app/GroupListItems.vue
+++ resources/js/Components/app/GroupListItems.vue
@@ -4,11 +4,12 @@ import TextInput from "@/Components/TextInput.vue";
 import GroupItem from "@/Components/app/GroupItem.vue";
 import {ref} from "vue";
 
-const searchKeyword = ref();
+const searchKeyword = ref('');
 </script>
 
 <template>
     <TextInput :model-value="searchKeyword" placeholder="Type to search group" class="w-full mt-4"/>
+
     <div class="mt-3 h-[200px] lg:flex-1 overflow-auto">
         <div v-if="false" class="text-gray-400 text-center p-3">
             You are not joined to any groups
